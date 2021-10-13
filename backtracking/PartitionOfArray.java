0f41f21929689c59ef98da053e3884769db4544e
VIVEK KUMAR OJHA <72193872+Vivek-775@users.noreply.github.com>
2021-10-13 12:46:31 +0530

    Update PartitionOfArray.java

--- backtracking/PartitionOfArray.java
+++ backtracking/PartitionOfArray.java
@@ -6,7 +6,7 @@ public class PartitionOfArray {
 
 	public static void main(String[] args) {
 
-		int a[] = {2, 1, 2, 3, 4, 8};
+		int a[] = {2, 1, 2, 3, 4, 8, 9};
 		int sum = 0;
 		for(int i = 0; i<a.length; i++) {
 			sum += a[i];
